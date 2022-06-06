6d85b868bf1a891022b0ed8c67ac3de7e8f55c04
Sana Zahlan <sanazahlan@yahoo.com>
2022-06-06 00:32:24 -0700

    Write function that returns response/error based on API call results.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,20 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import ExchangeRateService from './exchange-rate-service';
 
-// UI Logic
+$(document).ready(function() {
+  $('#conversion-form').submit(function(event) {
+    event.preventDefault();
+
+    let usd = parseInt($('#dollar-amount').val());
+    let requestedCurrency = $('input[name="currency"]:checked').val();
+    let promise = ExchangeRateService.getRate();
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      $('.showValue').text(`${usd} in ${requestedCurrency} is ${usd * body.conversion_rates.requestedCurrency}`);
+    }, function(error) {
+        $('.showErrors').text(`There was an error processing your request: ${error}`);
+    });
+  });
+});
