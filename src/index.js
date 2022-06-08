b1e26ef93de76590df966063ce37e57843661689
Sana Zahlan <sanazahlan@yahoo.com>
2022-06-08 13:31:32 -0700

    Clean up project and delete any debugging statements.

--- src/index.js
+++ src/index.js
@@ -2,22 +2,20 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import ExchangeRateService from './exchange-rate-service';
+import ExchangeRateService from './exchange-rate-service.js';
 
 $(document).ready(function() {
   $('#conversion-form').submit(function(event) {
     event.preventDefault();
 
-    let usd = parseInt($('#dollar-amount').val());
-    let requestedCurrency = $('input[name="currency"]:checked').val();
+    let usd = $('#dollar-amount').val();
+    let requestedCurrency = $("input[name='currency']:checked").val();
     let promise = ExchangeRateService.getRate();
-    // console.log(usd);
     
     promise.then(function(response) {
       const body = JSON.parse(response);
-      // console.log(body.conversion_rates.requestedCurrency);
-      // console.log(usd * body.conversion_rates.requestedCurrency);
-      const convertedValue = usd * parseInt(body.conversion_rates.requestedCurrency);
+      const convertedValue = usd * body.conversion_rates.requestedCurrency;
+  
       if (requestedCurrency === "SIM") {
         $('.showValue').text("Simoleon conversions are unfortunately not available on this page. We apologize for the inconvenience."); 
       } else {
